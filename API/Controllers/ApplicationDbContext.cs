60f851a60e1855a069b8c06b6e35a8b29d116727
TRANDOANCHITHANH <tranchithanh0404@gmail.com>
2024-09-30 00:41:27 +0700

    Add project files.

---
+++ API/Controllers/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options) 
+        {
+
+        }
+    }
+}
